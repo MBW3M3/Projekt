d28c8e322ee392af61f7d1348192e1311a92289f
MB <w3rt3zz@gmail.com>
2022-01-21 21:15:07 +0100

    Dodaje kod w c do imienia i nazwiska

---
+++ MikolajBialekIIM/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void iin(int b){
+    char imie[20];
+    char nazwisko[20];
+    printf("Podaj swoje imie:\n");
+    scanf("%s", &imie);
+    printf("Podaj swoje nazwisko:\n");
+    scanf("%s", &nazwisko);
+    printf("Twoje imie to %s a nazwisko %s", imie, nazwisko);
+    return 0;
+}
+int main() {
+    int a;
+    iin(a);
+    return 0;
+}
