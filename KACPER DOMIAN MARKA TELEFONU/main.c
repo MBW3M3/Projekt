838d68a466d167b04b3e349b6389cd2a1b11247a
Kacpertojaa <Kaspersssky@gmail.com>
2022-01-22 18:29:52 +0100

    Dodaje Marke

---
+++ KACPER DOMIAN MARKA TELEFONU/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+void powiadomienie(int liczbaPieter);
+int main(void)
+{
+    int marka;
+    printf ("Podaj marke telefonu:\n");
+    scanf("%d", &marka);
+
+    return 0;
+}
