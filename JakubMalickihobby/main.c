a22cb9d23566ac446b2bf5bba6d51ecafeecabe6
jakub-malicki <Jakubasmal@wp.pl>
2022-01-22 18:12:59 +0100

    dodaje hobby

---
+++ JakubMalickihobby/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char tablica[100];
+    printf("Podaj swoje hobby: \n");
+    scanf("%s" &tablica);
+    printf("Twoje hobby to: %s", tablica);
+    return 0;
+}
