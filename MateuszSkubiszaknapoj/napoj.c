8b23c7880dab89f1c0db01dfda9a39f04f6de147
MB <w3rt3zz@gmail.com>
2022-01-22 19:22:18 +0100

    Polaczylem wszystkie kody oraz poprawilem niektore zmienne.

--- MateuszSkubiszaknapoj/napoj.c
+++ MateuszSkubiszaknapoj/napoj.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int main() {
-char a[20];
-char b[20];
+char ngg[20];
+char sg[20];
+char ulub[30];
 printf("Jaki jest twoj ulubiony napoj wpisz gazowany czy nie gazowany\n");
-scanf("%s",a);
+scanf("%s",ngg);
 printf("Jaki smak wolisz slodki czy gorzki\n");
-scanf("%s",b);
-printf("Wolisz napój %s i %s", a, b);
+scanf("%s",sg);
+printf("Jaki jest twoj ulubiony napoj\n");
+scanf("%s", ulub);
+printf("Wolisz napoj: %s", ngg);
+Printf("Wolisz napoj: %s", sg);
+printf("A twoj ulubiony napoj to: %s", ulub);
 return 0;
 }
