ebe1a31fc96908514d1c60bb90537f15821b3c6a
MBW3M3 <98182330+MBW3M3@users.noreply.github.com>
2022-01-22 19:04:05 +0100

    Update main.c
    Zmieniłem kod, dodałem na później printf, który przyda się pod koniec

--- KACPER DOMIAN MARKA TELEFONU/main.c
+++ KACPER DOMIAN MARKA TELEFONU/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-void powiadomienie(int liczbaPieter);
-int main(void)
-{
-    int marka;
-    printf ("Podaj marke telefonu:\n");
-    scanf("%d", &marka);
 
+{
+    char marka[30];
+    printf ("Podaj marke telefonu:\n");
+    scanf("%s", &marka);
+    printf ("Twoja marka telefonu to: %s\n", marka);
     return 0;
 }
