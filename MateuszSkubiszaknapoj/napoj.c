d69ba71c1e41b373ba7a50ce53917f818ecf1fbc
Mateusz-Skubiszak <mateusz.skubiszak1@wp.pl>
2022-01-26 15:22:18 +0100

    Dodaje commit 2

--- MateuszSkubiszaknapoj/napoj.c
+++ MateuszSkubiszaknapoj/napoj.c
@@ -14,5 +14,6 @@ printf("Wolisz napoj: %s", ngg);
 Printf("Wolisz napoj: %s", sg);
 printf("A twoj ulubiony napoj to: %s", ulub);
 printf("Dodaje na potrzeby commita");
+printf("Dodaje na potrzeby commita2");
 return 0;
 }
